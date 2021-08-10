5a01f3b205f39832df17dcaa23a8ea5aeaa29410
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-08-10 10:03:23 -0500

    Finish Formik basic layout

--- src/components/FormDemo.tsx
+++ src/components/FormDemo.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
-import { Form, Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import React from 'react';
 import { InvestmentDetails } from '../types';
 
@@ -20,6 +20,26 @@ const FormDemo = () => {
 				<Formik initialValues={initialValues} onSubmit={() => {}}>
 					{({ values }) => (
 						<Form>
+							<Field name='fullName' />
+							<Field name='initialInvestment' type='number' />
+
+							<Field name='investmentRisk' value='High' type='checkbox' />
+							<Field name='investmentRisk' value='Mediun' type='checkbox' />
+							<Field name='investmentRisk' value='Low' type='checkbox' />
+
+							<Field name='commentAboutInvestmentRisk' as='textarea' />
+
+							<Field name='dependents' as='select'>
+								<option value={0}>0</option>
+								<option value={1}>1</option>
+								<option value={2}>2</option>
+								<option value={3}>3</option>
+								<option value={4}>4</option>
+								<option value={5}>5</option>
+							</Field>
+
+							<Field name='acceptedTermsAndConditions' type='checkbox' />
+
 							<pre>{JSON.stringify(values, null, 4)}</pre>
 						</Form>
 					)}
